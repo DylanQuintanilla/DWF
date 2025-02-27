d66f482e9eea097c214748035a45292e24297b2c
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-02-27 14:42:59 -0600

    Add files via upload

---
+++ PruebaDesafio/src/main/java/sv/edu/udb/PruebaDesafioApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDesafioApplication.class, args);
+	}
+
+}
