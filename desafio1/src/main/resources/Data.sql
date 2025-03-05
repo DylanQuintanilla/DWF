7762d4aa04ae7934ea382e958a83e27eaaa71e05
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-03-04 23:29:46 -0600

    aaa

--- desafio1/src/main/resources/Data.sql
+++ desafio1/src/main/resources/Data.sql
@@ -1,13 +1,13 @@
 -- Insertamos datos en la tabla ocupaciones (debes agregar ocupaciones válidas)
 INSERT INTO ocupaciones (id_ocupacion, nombre_ocupacion) VALUES
-                                                             (1, 'Ingeniero'),
-                                                             (3, 'Profesor');
+(1, 'Ingeniero'),
+(3, 'Profesor');
 
 -- Insertamos datos en la tabla persona
 INSERT INTO persona (id_persona, nombre_persona, edad_persona, telefono_persona, sexo_persona, id_ocupacion, fecha_nac) VALUES
-                                                                                                                            (25, 'Jose', 40, '77889966', 'Masculino', 3, '1940-04-08'),
-                                                                                                                            (27, 'Juan', 25, '77558899', 'Masculino', 1, '1990-05-20'),
-                                                                                                                            (28, 'Juan', 30, '12345678', 'Masculino', 1, '1990-05-20'),
-                                                                                                                            (29, 'Emerson', 25, '76150644', 'Masculino', 1, '1999-03-08');
+(25, 'Jose', 40, '77889966', 'Masculino', 3, '1940-04-08'),
+(27, 'Juan', 25, '77558899', 'Masculino', 1, '1990-05-20'),
+(28, 'Juan', 30, '12345678', 'Masculino', 1, '1990-05-20'),
+(29, 'Emerson', 25, '76150644', 'Masculino', 1, '1999-03-08');
 
 
